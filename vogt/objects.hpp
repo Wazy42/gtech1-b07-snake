a3ae6c96e2c443829b7a580417741e9ea8d3590f
nvogt <nvogt@gaming.tech>
2022-01-28 09:20:24 +0100

    updates files vogt

--- vogt/objects.hpp
+++ vogt/objects.hpp
@@ -1,11 +1,20 @@
+#ifndef includes
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 900
+#define TILE_SIZE 16
+#endif
+
 class Snake {
 public:
   int x;  // Position horizontale
   int y; // Position verticale
   Snake();
   ~Snake();
-  void changeDir(int num);
-  void move();
+  void changeDir(int dir);
+  void move(int dir = -1);
 private:
   int dirX; // Direction horizonale
   int dirY; // Direction vertiacale
