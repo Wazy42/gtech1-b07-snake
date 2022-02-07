37a9c04098ad8c919857c06205fe5531b9671ad9
Lucas <lvocanson@gaming.tech>
2022-02-07 15:57:07 +0100

    fin de séance 07/02

--- snake.cpp
+++ snake.cpp
@@ -43,7 +43,6 @@ Application::~Application(){
   delete this-> Apple;
   delete this-> Room;
   delete this-> main_window;
-      printf("DESTRUCTION DINIE\n");
   IMG_Quit();
   SDL_Quit();
 }
@@ -60,13 +59,15 @@ void Application::appLoop() {
     /// Events
     while (SDL_PollEvent(&this-> event)) { // While there is keyboard/mouse event in the queue
       switch (this-> event.type) {
-        case SDL_QUIT: // In case we hit the red X or Alt+f4
+        case SDL_QUIT: // In case we hit the red X
           interrupt = true;
+          break;
         case SDL_KEYDOWN: // We pressed a key on the keyboard
           if (this-> event.key.keysym.sym == SDLK_UP && lastDir%2 == 0) dir = DIR_UP; // Up arrow
           if (this-> event.key.keysym.sym == SDLK_DOWN && lastDir%2 == 0) dir = DIR_DOWN; // Down arrow
           if (this-> event.key.keysym.sym == SDLK_LEFT && lastDir%2 == 1) dir = DIR_LEFT; // Left arrow
           if (this-> event.key.keysym.sym == SDLK_RIGHT && lastDir%2 == 1) dir = DIR_RIGHT; // Right arrow
+          break;
       }
     }
     if (count%(frame_rate/snake_rate) == 0 && !interrupt) {
@@ -95,7 +96,7 @@ void Application::appLoop() {
 void Application::appInit() {
   /// Graphical init
   this-> main_window = new MainWindow();
-  this-> main_window-> init("ゲームをヘビ", SCREEN_WIDTH, SCREEN_HEIGHT);
+  this-> main_window-> init("Snake!", SCREEN_WIDTH, SCREEN_HEIGHT);
   this-> renderer = main_window-> getRenderer(); // Get renderer
   this-> Room = new Playground(renderer);
   
